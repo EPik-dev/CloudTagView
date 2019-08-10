5ed282aecf3d64751383f4a9a25defaa3d8289db
Eldar Pikunov <eldarpikunov@gmail.com>
2019-08-10 18:18:03 +0300

    Swift framework with shared scheme

---
+++ CloudTagView/CloudTagView.h
@@ -0,0 +1,19 @@
+//
+//  CloudTagView.h
+//  CloudTagView
+//
+//  Created by Eldar Pikunov on 10.08.2019.
+//  Copyright © 2019 Eldar Pikunov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CloudTagView.
+FOUNDATION_EXPORT double CloudTagViewVersionNumber;
+
+//! Project version string for CloudTagView.
+FOUNDATION_EXPORT const unsigned char CloudTagViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CloudTagView/PublicHeader.h>
+
+
